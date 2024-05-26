7cf8acc60f37c58b1d728a727b01351358aeebe7
Jason C <jason44406@yahoo.com>
2024-05-26 11:05:02 -0400

    expand quote system test

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -1,15 +1,12 @@
 require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  # ## Visiting the Quotes#index page
-  # ## we expect to see the heading "Quotes"
-  #   visit quotes_url
-  
-  #   assert_selector "h1", text: "Quotes"
-  # end
 
-  test "creating a new quote" do
+  setup do
+    @quote = quotes(:first)
+  end
+
+  test "creating a new quote from the index" do
     # When we visit the Quotes#index page
     # we expect to see the heading "Quotes"
     visit quotes_path
@@ -32,4 +29,41 @@ class QuotesTest < ApplicationSystemTestCase
     assert_text "My Quote"
   end
 
+  test "showing a quote from the index" do
+    visit quotes_path
+
+    # When we click on the name of the quote
+    # we expect to be taken to the Quotes#show page
+    # which has the title of the quote
+    click_link @quote.name
+    assert_selector "h1", text: @quote.name
+  end
+
+  test "updating a quote from the index" do
+    visit quotes_path
+
+    # When we click on the "Edit" link of the quote
+    # we expect to be taken to the Quotes#edit page
+    # which has the title "Editing Quote"
+    click_link "Edit", match: :first
+    assert_selector "h1", text: "Edit quote"
+
+    # When we fill in the name input with "My Updated Quote"
+    # and we click on the "Update Quote" button
+    fill_in "Name", with: "Updated quote"
+    click_on "Update quote"
+
+    # Then we expect to be taken to the Quotes#index page
+    # we expect to see the updated quote in the list of quotes
+    assert_selector "h1", text: "Quotes"
+    assert_text "Updated quote"
+  end
+
+  test "Destroying a quote" do
+    visit quotes_path
+    assert_text @quote.name
+
+    click_on "Delete", match: :first
+    assert_no_text @quote.name
+  end
 end
